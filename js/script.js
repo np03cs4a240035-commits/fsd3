5bbcd5b05bc20a1ceda2377258639442058364b2
Swekshya Rajbhandari <itsswekshya@gmail.com>
2025-12-07 01:55:25 +0545

    New code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // Correct API URL
 const API_URL = "https://69346d9e4090fe3bf01fe4c5.mockapi.io/movies";
 
-
 const movieListDiv = document.getElementById('movie-list');
 const searchInput = document.getElementById('search-input');
 const form = document.getElementById('add-movie-form');
